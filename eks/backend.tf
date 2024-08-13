1abb79db3cd607d3e1e688ec614a7167f14db7be
gani-2024 <roneethsai@gmail.com>
2024-08-13 15:45:35 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "s3-devopsified"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "Lock-terra-files"
     encrypt        = true
   }
 }
